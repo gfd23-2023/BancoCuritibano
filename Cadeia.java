287b0d8fc6a04461db75c9af222834de2a1ea52a
Nathalia <Nathlia>
2024-11-12 23:00:12 -0300

    Casas VaParaPrisao e VoltarPontoPartida

--- Cadeia.java
+++ Cadeia.java
@@ -37,7 +37,7 @@ public class Cadeia extends CasaExtra {
 
       opcao = scanner.nextInt();  
       if (opcao == 1) {
-        // Lógica para pagar 50 reais
+        // Lógica para pagar 50 reais (verificar dinheiro e, se sair, mudar estado naCadeia e zerar rodadasNaCadeia)
       } else if (opcao == 2) {
         // Verificar lista de cartas do jogador. Se tiver "Sair da prisao", remove a carta e tira ele da prisao.
       } else {
@@ -45,7 +45,17 @@ public class Cadeia extends CasaExtra {
       }
  
     } else if (origem.getRodadasNaCadeia() == 3) {
-      // Procedimento para tentar sair na terceira e ultima rodada
+      
+      System.out.println("Você pode tentar sair da prisão: ");
+      System.out.println("1 - Tentar tirar dupla nos dados");
+
+      opcao = scanner.nextInt();  
+      if (opcao == 1) {
+        // Lógica para sortear dois dados (Passar dados como parâmetro (?))
+      } else {
+        System.out.println("Opção invalida.");
+      }
+    
     }
   
     scanner.close();
---
+++ Jogador.java
@@ -0,0 +1,89 @@
+package main;
+
+import java.io.*;
+
+public class Jogador {
+  private String nome;
+  private String foto;
+  private double dinheiro;
+  private int casaAtual;
+  private boolean falido;
+  private boolean naCadeia;
+  private int rodadasNaCadeia;
+
+  /* Construtores */
+  Jogador() {};
+
+  Jogador(String nome, String foto) {
+    this.nome = nome;
+    this.foto = foto;
+    this.dinheiro = (double) 2000;
+    this.casaAtual = 0;
+    this.falido = false;
+    this.naCadeia = false;
+    this.rodadasNaCadeia = 0;
+  }
+
+  /* Getters e Setters */
+  String getNome() {
+    return nome;
+  }
+
+  void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  String getFoto() {
+    return foto;
+  }
+
+  void setFoto(String foto) {
+    this.foto = foto;
+  }
+
+  double getDinheiro() {
+    return dinheiro;
+  }
+
+  void setDinheiro(double dinheiro) {
+    this.dinheiro = dinheiro;
+  }
+
+  int getCasa() {
+    return casaAtual;
+  }
+
+  void setCasa(int casaNova) {
+    this.casaAtual = casaNova;
+  }
+
+  boolean ehFalido() {
+    return falido;
+  }
+
+  void setFalido() {
+    falido = true;
+  }
+
+  public boolean estaNaCadeia() {
+    return naCadeia;
+  }
+
+  public void setNaCadeia(boolean naCadeia) {
+    this.naCadeia = naCadeia;
+  }
+
+  public int getRodadasNaCadeia() {
+    return rodadasNaCadeia;
+  }
+
+  public void setRodadasNaCadeia(int zerarOuAumentar) {
+    if (zerarOuAumentar == 0) {
+      rodadasNaCadeia = 0;
+    } else if (zerarOuAumentar == 1) {
+      rodadasNaCadeia++;
+    } else {
+      System.out.println("Erro. Código zerarOuAumentar inválido.");
+    }
+  }
+} 
